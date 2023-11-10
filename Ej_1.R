cb7d1b977099b81ff6daa1ff90a34ae15c50f289
Cande <cande.russof2@gmail.com>
2023-11-10 19:08:23 -0300

    Ej 1

---
+++ Ej_1.R
@@ -0,0 +1,21 @@
+#Practica 4. Ejercicio 1
+
+rm(list=ls())
+setwd("/Users/cande/Desktop/Labo/Practica_4/")
+
+#El archivo de datos 87576.dat es un archivo ascii. Abrir el archivo 87576.dat en 
+#modo solo lectura.
+
+#-------------------------------------------------------------------------------
+#a) 
+#Verificar que la apertura del archivo fue exitosa (impedir que el codigo se 
+#interrumpa si el archivo no se encuentra, en este caso el script debe informar
+#que el archivo no fue encontrado).
+
+
+#-------------------------------------------------------------------------------
+b) Asignar los valores de la columna 2 a la variable ‘Td’ y el contenido de la columna 3 a
+la variable ‘Temperatura’.
+c) Leer s´olo las primeras 20 l´ıneas del archivo almacenando la primera columna en una
+variable integer, y la segunda y tercera columna en una variable real.
+d) Leer s´olo las primeras 20 l´ıneas y las primeras 3 columnas.
